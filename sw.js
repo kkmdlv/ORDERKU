624739c95f61ed2bec2c6d8027ec199112c0d31e
kukami <kukamidata@gmail.com>
2026-05-27 13:26:45 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,39 @@
-const CACHE_NAME = "kukami-v1";
+const CACHE_NAME = "kukami-v3";
 
 const urlsToCache = [
+
   "./",
   "./index.html",
   "./style.css",
-  "./script.js"
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./logo (2).png"
+
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event)=>{
 
   event.waitUntil(
 
     caches.open(CACHE_NAME)
-    .then(cache => {
+    .then((cache)=>{
+
       return cache.addAll(urlsToCache);
+
     })
 
   );
 
 });
 
-
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event)=>{
 
   event.respondWith(
 
     caches.match(event.request)
-    .then(response => {
+    .then((response)=>{
 
       return response || fetch(event.request);
 
